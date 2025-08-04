16f9a31b12c1a5b059fdb7b5540f1d48d91fecbc
Kai-Barker <241065@virtualwindow.co.za>
2025-08-05 00:34:27 +0200

    fixed spacing

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,75 +9,27 @@ import FriendsIcon from "../assets/images/FriendsIcon.png";
 
 const Navbar = () => {
   return (
-    // <Container>
-    //   <Row>
-    //     <Col>1 of 2</Col>
-    //     <Col>2 of 2</Col>
-    //   </Row>
-    //   <Row>
-    //     <Col>1 of 3</Col>
-    //     <Col>2 of 3</Col>
-    //     <Col>3 of 3</Col>
-    //   </Row>
-    // </Container>
-    <div className="navContainer">
-      <Container fluid style={{ marginLeft: "1vw" }}>
-        <Row>
-          <Col lg={{ span: 2, offset: 0 }}>
-            <img src={Logo} style={{ maxWidth: "60%", marginBottom:'1vh',marginTop:'1vh' }} />
+    <div className="navContainer py-2">
+      <Container fluid>
+        <Row className="align-items-center">
+          {/* Logo */}
+          <Col lg={2}>
+            <img src={Logo} style={{ maxWidth: "60%" }} />
           </Col>
-          <Col
-            lg={{ span: "auto", offset: 1 }}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <div>
-              <Link className="nav-links" to="/">
-                Home
-              </Link>
-            </div>
-            <div style={{marginLeft:'3vw'}}>
-              <Link className="nav-links" to="/">
-                Browse Posts
-              </Link>
-            </div>
-            <div style={{marginLeft:'3vw'}}>
-              <Link className="nav-links" to="/">
-                My Posts
-              </Link>
-            </div>
-            <div style={{marginLeft:'3vw'}}>
-              <Link className="nav-links" to="/">
-                Categories
-              </Link>
-            </div>
+
+          {/* Nav Links */}
+          <Col lg={{span:5, offset:1}} className="d-flex justify-content-center gap-5">
+            <Link className="nav-links" to="/">Home</Link>
+            <Link className="nav-links" to="/">Browse Posts</Link>
+            <Link className="nav-links" to="/">My Posts</Link>
+            <Link className="nav-links" to="/">Categories</Link>
           </Col>
-          <Col
-            lg={{ span: "3", offset: 1 }}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <div>
-              <Link className="nav-links" to="/">
-                Help
-              </Link>
-            </div>
-            <div>
-              <Link to="/">
-                <img src={FriendsIcon} style={{ maxWidth: "40%", marginLeft:'3vw' }} />
-              </Link>
-            </div>
-            <div>
-              <Link to="/">
-                <img src={icon} style={{ marginLeft:'1vw' }} className="profileIcon"/>
-              </Link>
-            </div>
+
+          {/* Icons */}
+          <Col lg={{span:3,offset:1}} className="d-flex justify-content-end align-items-center gap-4 pe-4">
+            <Link className="nav-links" to="/">Help</Link>
+            <Link to="/"><img src={FriendsIcon} style={{ height: "50px" }} /></Link>
+            <Link to="/"><img src={icon} className="profileIcon" style={{maxHeight:'80%'}}/></Link>
           </Col>
         </Row>
       </Container>
